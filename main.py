bdf9bd575a008eddfd36401f226b79aa42559671
Aditya Gulati <adityagulatiadityagulati@gmail.com>
2020-08-26 00:52:28 +0530

    First commit

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 # -*- coding: utf-8 -*-
 
-from resources.lib import kodilogging
-from resources.lib import script
-
-import logging
 import xbmcaddon
-
-# Keep this file to a minimum, as Kodi
-# doesn't keep a compiled copy of this
-ADDON = xbmcaddon.Addon()
-kodilogging.config()
-
-script.show_dialog()
+import xbmcgui
+ 
+addon       = xbmcaddon.Addon()
+addonname   = addon.getAddonInfo('name')
+ 
+line1 = "Hello World!"
+line2 = "We can write anything we want here"
+line3 = "Using Python"
+ 
+xbmcgui.Dialog().ok(addonname, line1, line2, line3) 
 
 
